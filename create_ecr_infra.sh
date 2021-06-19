fc75ab65094aafdb1c5f597156915b0d420f82fa
Rene-Perlego <rene@perlego.com>
2021-06-19 20:57:57 +0100

    added ecr cf deployment yaml

---
+++ create_ecr_infra.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "Deploying AWS ECR Repository.."
+
+aws cloudformation deploy \
+              --template-file ./infrastructure/aws_cf_ecr.yaml \
+              --stack-name bcrypt-ecr-repo
+
+echo "ECR deployment successfull."
